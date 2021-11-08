98dc91cdf00d1e6672d592cbc0db59fd9e31fe95
Daniel Lorenzo <juandaniellorenzocabrera@alumno.ieselrincon.es>
2021-11-08 19:29:07 +0000

    creada hoja carrito js

--- index.php
+++ index.php
@@ -41,11 +41,9 @@
     </div>
     
     <?php
-        if(isset($_SESSION)){
+        /*if(isset($_SESSION)){
             var_dump($_SESSION);
-        }
-
-?>
+        } */?>
     
 </body>
 </html>
